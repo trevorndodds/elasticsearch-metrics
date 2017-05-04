a1634d1311cf3a87557fe35ab479439f4039343e
Trevor Dodds <trevor.dodds@gmail.com>
2017-05-04 14:50:26 -0400

    Fixed bugs, added stats_code for cluster health
    fetch_clusterhealth was being called twice from last commit
    removed argparse in favor of environment variables
    added status codes for new dashboard

--- Grafana/elasticsearch2elastic.py
+++ Grafana/elasticsearch2elastic.py
@@ -6,24 +6,14 @@ import json
 import urllib2
 import os
 import sys
-import argparse
-
 
 # ElasticSearch Cluster to Monitor
 elasticServer = os.environ.get('ES_METRICS_CLUSTER_URL', 'http://server1:9200')
-interval = 60
+interval = os.environ.get('ES_METRICS_INTERVAL', 60)
 
 # ElasticSearch Cluster to Send Metrics
 elasticIndex = os.environ.get('ES_METRICS_INDEX_NAME', 'elasticsearch_metrics')
 elasticMonitoringCluster = os.environ.get('ES_METRICS_MONITORING_CLUSTER_URL', 'http://server2:9200')
-
-
-## Parse argument
-def parse_args():
-  parser = argparse.ArgumentParser(description='Process some integers.')
-  parser.add_argument('--interval', help='The interval send to ES, unit is second')
-  args = parser.parse_args()
-  return args
 
 
 def fetch_clusterhealth():
@@ -35,11 +25,17 @@ def fetch_clusterhealth():
         jsonData = json.loads(response.read())
         clusterName = jsonData['cluster_name']
         jsonData['@timestamp'] = str(utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
+        if jsonData['status'] == 'green':
+            jsonData['status_code'] = 0
+        elif jsonData['status'] == 'yellow':
+            jsonData['status_code'] = 1
+        elif jsonData['status'] == 'red':
+            jsonData['status_code'] = 2
         post_data(jsonData)
         return clusterName
     except IOError as err:
         print "IOError: Maybe can't connect to elasticsearch."
-        clusterName="unknown"
+        clusterName = "unknown"
         return clusterName
 
 
@@ -84,11 +80,8 @@ def fetch_indexstats(clusterName):
 
 def post_data(data):
     utc_datetime = datetime.datetime.utcnow()
-    url_parameters = {
-        'cluster': elasticMonitoringCluster,
-        'index': elasticIndex,
-        'index_period': utc_datetime.strftime("%Y.%m.%d"),
-    }
+    url_parameters = {'cluster': elasticMonitoringCluster, 'index': elasticIndex,
+        'index_period': utc_datetime.strftime("%Y.%m.%d"), }
     url = "%(cluster)s/%(index)s-%(index_period)s/message" % url_parameters
     headers = {'content-type': 'application/json'}
     try:
@@ -100,40 +93,31 @@ def post_data(data):
 
 def main():
     clusterName = fetch_clusterhealth()
-    fetch_clusterstats()
-    fetch_nodestats(clusterName)
-    fetch_indexstats(clusterName)
+    if clusterName != "unknown":
+        fetch_clusterstats()
+        fetch_nodestats(clusterName)
+        fetch_indexstats(clusterName)
+
 
 if __name__ == '__main__':
-    # Parse argument
-    args = parse_args()
-
-    # If don't give interval paramter , default interval is 60s
-    if args.interval is not None:
-        interval = float(args.interval)
-
-    print "Interval: " + str(interval) + " s"
-
     try:
         nextRun = 0
         while True:
-                if time.time() >= nextRun:
-                        nextRun = time.time() + interval
-                        now = time.time()
-                        if fetch_clusterhealth() != "unknown":
-                            main()
-                            elapsed = time.time() - now
-                            print "Total Elapsed Time: %s" % elapsed
-                            timeDiff = nextRun - time.time()
+            if time.time() >= nextRun:
+                nextRun = time.time() + interval
+                now = time.time()
+                main()
+                elapsed = time.time() - now
+                print "Total Elapsed Time: %s" % elapsed
+                timeDiff = nextRun - time.time()
 
-                            # Check timediff , if timediff >=0 sleep, if < 0 send metrics to es
-                            if timeDiff >= 0:
-                                  time.sleep(timeDiff)
- 
+                # Check timediff , if timediff >=0 sleep, if < 0 send metrics to es
+                if timeDiff >= 0:
+                    time.sleep(timeDiff)
+
     except KeyboardInterrupt:
         print 'Interrupted'
         try:
             sys.exit(0)
         except SystemExit:
-            os._exit(0)
-
+            os._exit(0) 
